84fa5d79f6f1eef6af9e87cd42928f5060653fd9
Georgie Ivanov <georgie@barimax.bg>
2026-03-15 14:52:23 +0200

    initial commit

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "csMessageTypes",
+    name: "cs-message-types",
     platforms: [
         .macOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "csMessageTypes",
-            targets: ["csMessageTypes"]
+            name: "CSMessageTypes",
+            targets: ["CSMessageTypes"]
         ),
     ],
     dependencies: [
@@ -22,11 +22,13 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "csMessageTypes",
+            name: "CSMessageTypes",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
-            ]
+            ],
+            swiftSettings: [.enableUpcomingFeature("ExistentialAny")]
         ),
+        
 
     ]
 )
