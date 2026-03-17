eb51952dadb49ae40e4848003f872eea13ae95ee
Georgie Ivanov <georgie@barimax.bg>
2026-03-17 12:38:35 +0200

    initial commit

--- Sources/CSMessageTypes/messages.swift
+++ Sources/CSMessageTypes/messages.swift
@@ -42,6 +42,10 @@ extension Message {
 
 }
 
+public protocol WebsocketContainerEncodable: Content {
+    func toWebsocketContainer() -> WebsocketContainer
+}
+
 public enum WebsocketSeverity: String, Codable, Sendable {
     public static let name: String = "WebsocketMessageSeverity"
     case info
@@ -50,7 +54,7 @@ public enum WebsocketSeverity: String, Codable, Sendable {
     case success
 }
 
-public struct WebsocketNotification: Message {
+public struct WebsocketNotification: Message, WebsocketContainerEncodable {
     public static let urlPath: String = "notification"
     public let userId: UUID
     public let title: String
@@ -67,9 +71,13 @@ public struct WebsocketNotification: Message {
         self.sessionId = sessionId
         self.companyId = companyId
     }
+    
+    public func toWebsocketContainer() -> WebsocketContainer {
+        .init(notification: self)
+    }
 }
 
-public struct WebsocketTextMessage: Message {
+public struct WebsocketTextMessage: Message, WebsocketContainerEncodable {
     public static let urlPath: String = "message/text"
     public let userId: UUID
     public let title: String
@@ -86,9 +94,13 @@ public struct WebsocketTextMessage: Message {
         self.sessionId = sessionId
         self.companyId = companyId
     }
+    
+    public func toWebsocketContainer() -> WebsocketContainer {
+        .init(textMessage: self)
+    }
 }
 
-public struct WebsocketProgress: Message {
+public struct WebsocketProgress: Message, WebsocketContainerEncodable {
     public static let urlPath: String = "progress"
     public let userId: UUID
     public let progress: Double
@@ -100,5 +112,82 @@ public struct WebsocketProgress: Message {
         self.progress = progress
         self.sessionId = sessionId
         self.companyId = companyId
+    }
+    
+    public func toWebsocketContainer() -> WebsocketContainer {
+        .init(progress: self)
+    }
+}
+
+public enum WebsocketMessageType: String, Codable, Sendable {
+    public static let name: String = "WebsocketMessageType"
+    case text
+    case object
+}
+
+public enum WebsocketMessageStatus: String, Codable, Sendable {
+    public static let name: String = "WebsocketMessageStatus"
+    case unread
+    case read
+    case notSent
+}
+
+public struct WebsocketContainer: Content {
+    public let title: String? // message and notification only
+    public let content: String? // message and notification only
+    public let severity: WebsocketSeverity
+    public let sessionId: String
+    public let companyId: UUID?
+    public let progress: Double?  // progress only
+    public let type: WebsocketMessageType? // message only
+    public let createdAt: Date?; // message only
+    public let lastUpdatedAt: Date? // message only
+    public let status: WebsocketMessageStatus? // message only
+    public let decodeAs: String?  // message only
+    public let object: String? ; // message only
+    
+    init(notification: WebsocketNotification) {
+        self.title = notification.title
+        self.content = notification.content
+        self.sessionId = notification.sessionId
+        self.severity = notification.severity
+        self.companyId = notification.companyId
+        self.createdAt = Date.now
+        self.lastUpdatedAt = Date.now
+        self.progress = nil
+        self.type = nil
+        self.status = nil
+        self.decodeAs = nil
+        self.object = nil
+    }
+    
+    init(textMessage: WebsocketTextMessage) {
+        self.title = textMessage.title
+        self.content = textMessage.content
+        self.sessionId = textMessage.sessionId
+        self.severity = textMessage.severity
+        self.companyId = textMessage.companyId
+        self.createdAt = Date.now
+        self.lastUpdatedAt = Date.now
+        self.progress = nil
+        self.type = .text
+        self.status = nil
+        self.decodeAs = nil
+        self.object = nil
+    }
+    
+    init(progress: WebsocketProgress) {
+        self.title = nil
+        self.content = nil
+        self.sessionId = progress.sessionId
+        self.severity = .info
+        self.companyId = progress.companyId
+        self.createdAt = nil
+        self.lastUpdatedAt = nil
+        self.progress = progress.progress
+        self.type = .text
+        self.status = nil
+        self.decodeAs = nil
+        self.object = nil
     }
 }
