ebc22e0029afc3519ed4bd0ed41215a1a31b5d4e
Georgie Ivanov <georgie@barimax.bg>
2026-03-17 10:07:22 +0200

    initial commit

---
+++ Sources/CSMessageTypes/SendingState.swift
@@ -0,0 +1,25 @@
+//
+//  SendingState.swift
+//  cs-message-types
+//
+//  Created by Georgie Ivanov on 17.03.26.
+//
+import Vapor
+import Foundation
+
+enum SendingState: AsyncResponseEncodable {
+    func encodeResponse(for request: Vapor.Request) async throws -> Vapor.Response {
+        switch self {
+        case .notFound:
+            return .init(status: .notFound)
+        case .success(let id):
+            return .init(status: .ok, body: .init(stringLiteral: id.uuidString))
+        case .error(let error):
+            return .init(status: .custom(code: 509, reasonPhrase: "Sending error"), body: .init(stringLiteral: error))
+        }
+    }
+    
+    case success(UUID)
+    case notFound
+    case error(String)
+}
